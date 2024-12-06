6154b2c11b90703cf482b4ca2a339722cd4f08e6
mrpucacimunyanyo <mrpucacimunyanyo@gmail.com>
2024-12-06 15:55:01 +0700

    menyelesaikan website kedai kopi part 2, fitur yang ditambahkan searchbox, sidebar product yang dibeli, dan modal detail product. style masih meniru belum ada modifikasi sendiri

--- js/script.js
+++ js/script.js
@@ -1,13 +1,65 @@
 const navbarNav = document.querySelector(".navbar-nav")
-
 const hamburgerMenu = document.getElementById("hamburger-menu")
+const search = document.querySelector('.navbar-extra a#search')
+const searchForm = document.querySelector('.search-form')
+const searchBox = document.getElementById('search-box')
+const shoppingCart = document.getElementById('shopping-cart')
+const shoppingCartMenu = document.querySelector('.shopping-card-menu')
+const tombolDetailProduk = document.querySelectorAll('.item-detail-btn')
+const closeDetailProduk = document.querySelector('.modal-container .close')
+const modal = document.getElementById('item-detail-modal')
+const modalContainer = document.querySelector('.modal-container')
 
+// ? responsifitas hamburger menu jika diklik menampilkan list daftar isi
 hamburgerMenu.onclick = () => {
     navbarNav.classList.toggle('active')
 }
 
+// ? fitur jika mengklik semua tempat selain daerah hamburger menu dan listnya akan memenghilangkan class active.
 document.addEventListener('click', function(e){
     if(!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)){
         navbarNav.classList.remove('active')
     }
 })
+
+
+search.addEventListener('click',function(e){
+    searchForm.classList.toggle('active')
+    searchBox.focus();
+    e.preventDefault();
+}) 
+
+document.addEventListener('click', function(e){
+    if(!search.contains(e.target) && !searchForm.contains(e.target)){
+        searchForm.classList.remove('active')
+    }
+})
+
+shoppingCart.onclick = (e) => {
+    shoppingCartMenu.classList.toggle('active')
+    e.preventDefault()
+}
+
+document.addEventListener('click', function(e){
+    if(!shoppingCart.contains(e.target) && !shoppingCartMenu.contains(e.target)){
+        shoppingCartMenu.classList.remove('active')
+    }
+})
+
+tombolDetailProduk.forEach((btn) => {
+    btn.onclick = (e) => {
+        modal.style.display = 'flex'
+        e.preventDefault()
+    }
+});
+
+window.onclick = (e) => {
+    if(e.target === modal){
+        modal.style.display = 'none'
+    }
+}
+
+closeDetailProduk.onclick = (e) => {
+        modal.style.display = 'none';
+        e.preventDefault();
+    }
